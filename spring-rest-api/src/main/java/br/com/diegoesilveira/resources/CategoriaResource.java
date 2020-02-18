b3a73a9ad02988dada8f3f79d235744a64cb3f6e
diego <diego@DESKTOP-6LKLR8F>
2020-02-17 21:57:56 -0300

    Criando projeto spring rest api.

---
+++ spring-rest-api/src/main/java/br/com/diegoesilveira/resources/CategoriaResource.java
@@ -0,0 +1,16 @@
+package br.com.diegoesilveira.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping(value = "/categorias")
+@RestController
+public class CategoriaResource {
+
+	@RequestMapping(method= RequestMethod.GET)
+	public String listar() {
+		return "Rest está funcionando";
+	}
+	
+}
